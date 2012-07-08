24af397dd7e76c031466b4dcce435fb0dfb22e99
Bob Jacobsen <rgj1927@pacbell.net>
2012-07-08 17:49:29 +0000

    make branch for 2.99.8 release

---
+++ 2.99.8/java/src/jmri/jmrix/ecos/EcosSensor.java
@@ -0,0 +1,58 @@
+// EcosSensor.java
+
+package jmri.jmrix.ecos;
+
+import jmri.implementation.AbstractSensor;
+
+/**
+ * Implement a Sensor via Ecos communications.
+ * <P>
+ * This object doesn't listen to the Ecos communications.  This is because
+ * it should be the only object that is sending messages for this sensor;
+ * more than one Sensor object pointing to a single device is not allowed.
+ *
+ * @author Kevin Dickerson (C) 2009
+ * @version	$Revision$
+ */
+public class EcosSensor extends AbstractSensor {
+
+    final static String prefix = "US";
+
+    int objectNumber = 0;
+
+    public EcosSensor(String systemName, String userName) {
+        super(systemName, userName);
+        init(systemName);
+    }
+
+    public EcosSensor(String systemName) {
+        super(systemName);
+        init(systemName);
+    }
+    
+    private void init(String id) { }
+    
+    void setObjectNumber(int o) { 
+        objectNumber = o;
+
+    }
+
+    public void requestUpdateFromLayout(){ }
+
+    static String[] modeNames = null;
+    static int[] modeValues = null;
+        
+    public int getObject() { return objectNumber; }
+
+    EcosReporter reporter = null;
+    
+    public void setReporter(EcosReporter er){
+        reporter = er;
+    }
+ 
+    static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(EcosSensor.class.getName());
+}
+
+/* @(#)EcosSensor.java */
+
+
---
+++ 2.99.8/java/src/jmri/jmrix/openlcb/OpenLcbMenu.java
@@ -0,0 +1,59 @@
+// OpenLcbMenu.java
+
+package jmri.jmrix.openlcb;
+import jmri.jmrix.can.swing.CanNamedPaneAction;
+
+import java.util.ResourceBundle;
+
+import javax.swing.JMenu;
+
+/**
+ * Create a menu containing the JMRI OpenLCB-specific tools
+ *
+ * @author	Bob Jacobsen   Copyright 2010
+ * @version     $Revision$
+ */
+public class OpenLcbMenu extends JMenu {
+    public OpenLcbMenu(jmri.jmrix.can.CanSystemConnectionMemo memo) {
+        super();
+        
+        ResourceBundle rb = ResourceBundle.getBundle("jmri.jmrix.JmrixSystemsBundle");
+        
+        String title;
+        if (memo != null)
+            title = memo.getUserName();
+        else
+            title = rb.getString("MenuItemCAN");
+        
+            setText(title);
+        
+        jmri.util.swing.WindowInterface wi = new jmri.util.swing.sdi.JmriJFrameInterface();
+        
+        for (Item item : panelItems) {
+            if (item == null) {
+                add(new javax.swing.JSeparator());
+            } else {
+                add(new CanNamedPaneAction( rb.getString(item.name), wi, item.load, memo));
+            }
+        }
+    }
+    
+        Item[] panelItems = new Item[] {
+            new Item("MenuItemConsole", "jmri.jmrix.openlcb.swing.monitor.MonitorPane"),
+            new Item("MenuItemSendFrame",   "jmri.jmrix.openlcb.swing.send.OpenLcbCanSendPane"),
+            new Item("MenuItemConfigNodes", "jmri.jmrix.openlcb.swing.networktree.NetworkTreePane"),
+        };
+    
+    static class Item {
+        Item(String name, String load) {
+            this.name = name;
+            this.load = load;
+        }
+
+        String name;
+        String load;
+    }
+
+}
+
+
