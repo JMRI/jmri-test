94a388bb7d7ce4fc954eab300809826d78c874ea
Bob Jacobsen <rgj1927@pacbell.net>
2012-12-14 16:08:08 +0000

    start release

---
+++ 3.2/java/src/jmri/jmrit/operations/trains/TrainManifestOptionAction.java
@@ -0,0 +1,41 @@
+//TrainManifestOptionAction.java
+
+package jmri.jmrit.operations.trains;
+
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+
+
+/**
+ * Swing action to load the train manifest options frame.
+ * 
+ * @author Bob Jacobsen Copyright (C) 2001
+ * @author Daniel Boudreau Copyright (C) 2010
+ * @version $Revision$
+ */
+public class TrainManifestOptionAction extends AbstractAction {
+	
+	TrainEditFrame frame = null;
+
+    public TrainManifestOptionAction(String s, TrainEditFrame frame) {
+    	super(s);
+    	this.frame = frame;
+    }
+
+    TrainManifestOptionFrame f = null;
+    public void actionPerformed(ActionEvent e) {
+        // create a settings frame
+    	if (f == null || !f.isVisible()){
+    		f = new TrainManifestOptionFrame();
+    		f.initComponents(frame);
+    	}
+        f.setExtendedState(Frame.NORMAL);
+        f.setVisible(true);	
+    }
+    
+	static org.apache.log4j.Logger log = org.apache.log4j.Logger
+	.getLogger(TrainManifestOptionAction.class.getName());
+}
+
+/* @(#)TrainManifestOptionAction.java */
---
+++ 3.2/java/src/jmri/jmrix/libusb/UsbViewAction.java
@@ -0,0 +1,34 @@
+// UsbViewAction.java
+
+package jmri.jmrix.libusb;
+
+/**
+ * Invoke the UsbView tool from libusb-java.
+ *
+ * @author   Bob Jacobsen Copyright 2008
+ * @version	$Revision$
+ */
+public class UsbViewAction extends javax.swing.AbstractAction {
+
+    public UsbViewAction(String s) { 
+        super(s);
+    }
+
+    public UsbViewAction() {
+        this(java.util.ResourceBundle.getBundle("jmri.jmrix.libusb.UsbViewActionBundle").getString("USB_Device_Viewer"));
+    }
+
+    public void actionPerformed(java.awt.event.ActionEvent e) {
+        // create and display
+        try{
+            new ch.ntb.usb.usbView.UsbView().setVisible(true);
+        } catch (java.lang.UnsatisfiedLinkError ex){
+            log.error(ex.toString());
+            javax.swing.JOptionPane.showMessageDialog(null, "Unable to find the libusb-win32 package.\nFor more details on how to installed it please check http://www.jmri.org/install/USB.shtml");
+        }
+    }
+    
+    static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(UsbViewAction.class.getName());
+}
+
+/* @(#)UsbViewAction.java */
