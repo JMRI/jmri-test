b623832301c70ccf85b015d7d1fdc55b77e5ec03
Randall Wood <randall.h.wood@alexandriasoftware.com>
2013-03-22 09:58:05 +0000

    Add existing NetBeans modules

---
+++ modules/core/src/org/jmri/app/Bundle.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.jmri.app;
+
+import edu.umd.cs.findbugs.annotations.Nullable;
+
+/**
+ *
+ * @author rhwood
+ */
+public class Bundle extends apps.Bundle {
+
+    private final static String name = "org.jmri.app.Bundle"; // NOI18N
+
+    //
+    // below here is boilerplate to be copied exactly
+    //
+    
+    /**
+     * Provides a translated string for a given 
+     * key from the package resource bundle or 
+     * parent.
+     *<p>
+     * Note that this is intentionally package-local
+     * access.
+     * 
+     * @param key Bundle key to be translated
+     * @return Internationalized text
+     */
+    static String getMessage(String key) {
+        return b.handleGetMessage(key);
+    }
+    /**
+     * Merges user data with a translated string for a given 
+     * key from the package resource bundle or 
+     * parent.
+     *<p>
+     * Uses the transformation conventions of 
+     * the Java MessageFormat utility.
+     *<p>
+     * Note that this is intentionally package-local
+     * access.
+     *
+     * @see java.text.MessageFormat
+     * @param key Bundle key to be translated
+     * @param subs One or more objects to be inserted into the message
+     * @return Internationalized text
+     */
+    static String getMessage(String key, Object ... subs) {
+        return b.handleGetMessage(key, subs);
+    }
+   
+    private final static apps.Bundle b = new apps.Bundle();
+    @Override @Nullable protected String bundleName() {return name; }
+    @Override protected jmri.Bundle getBundle() { return b; }
+    @Override protected String retry(String key) { return super.getBundle().handleGetMessage(key); }
+
+}
