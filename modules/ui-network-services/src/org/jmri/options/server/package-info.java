8cf344197cb580d8834f13610034d6fdf4abaf05
Randall Wood <randall.h.wood@alexandriasoftware.com>
2014-10-31 10:40:55 +0000

    Add options/preferences panel for servers. Add wrapper panel for existing web server preferences that is displayed as a sub-panel in the servers options/preferences panel.

---
+++ modules/ui-network-services/src/org/jmri/options/server/package-info.java
@@ -0,0 +1,6 @@
+@OptionsPanelController.ContainerRegistration(id = "NetworkServices", categoryName = "#OptionsCategory_Name_NetworkServices", iconBase = "org/jmri/options/server/Gnome-network-server.png", keywords = "#OptionsCategory_Keywords_NetworkServices", keywordsCategory = "NetworkServices")
+@NbBundle.Messages(value = {"OptionsCategory_Name_NetworkServices=Servers", "OptionsCategory_Keywords_NetworkServices=Network, Servers"})
+package org.jmri.options.server;
+
+import org.netbeans.spi.options.OptionsPanelController;
+import org.openide.util.NbBundle;
