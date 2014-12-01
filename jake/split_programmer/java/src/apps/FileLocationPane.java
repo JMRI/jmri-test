1f2d6ebdc39816f4f3b5f60c0385e5ead18cd967
Bob Jacobsen <rgj1927@pacbell.net>
2014-12-01 07:49:02 +0000

    start release

---
+++ jake/split_programmer/java/src/apps/FileLocationPane.java
@@ -0,0 +1,171 @@
+// FileLocationPane.java
+
+package apps;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.util.ResourceBundle;
+import javax.swing.AbstractAction;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import jmri.swing.PreferencesPanel;
+import jmri.util.FileUtil;
+
+/**
+ * Provide GUI to configure the Default File Locations
+ * <P>
+ * Provides GUI configuration for the default file locations by
+ * displaying textfields for the user to directly enter in their own path or
+ * a Set button is provided so that the user can select the path.
+ *
+ * @author      Kevin Dickerson   Copyright (C) 2010
+ * @version	$Revision$
+ */
+ 
+public class FileLocationPane extends JPanel implements PreferencesPanel {
+
+    protected static final ResourceBundle rb = ResourceBundle.getBundle("apps.AppsConfigBundle");
+    private boolean isDirty = false;
+
+    public FileLocationPane() {
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+    
+
+        add(PrefLocation());
+        add(ScriptsLocation());
+        
+        /*p = new JPanel();
+        JLabel throttle = new JLabel("Default Throttle Location");
+        p.add(throttle);
+        p.add(throttleLocation);
+        throttleLocation.setColumns(20);
+        throttleLocation.setText(jmri.jmrit.throttle.ThrottleFrame.getDefaultThrottleFolder());
+        add(p);*/
+        
+    }
+    
+    public static void save(){
+        FileUtil.setScriptsPath(scriptLocation.getText());
+        FileUtil.setUserFilesPath(userLocation.getText());
+        //jmri.jmrit.throttle.ThrottleFrame.setDefaultThrottleLocation(throttleLocation.getText());
+    }
+    
+    private JPanel ScriptsLocation(){
+        JButton bScript = new JButton(rb.getString("ButtonSetDots"));
+        final JFileChooser fcScript;
+        fcScript = new JFileChooser(FileUtil.getScriptsPath());
+
+        fcScript.setDialogTitle(rb.getString("MessageSelectDirectory"));
+        fcScript.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        fcScript.setAcceptAllFileFilterUsed(false);
+        bScript.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // get the file
+                fcScript.showOpenDialog(null);
+                if (fcScript.getSelectedFile()==null) return; // cancelled
+                scriptLocation.setText(fcScript.getSelectedFile()+File.separator);
+                isDirty = true;
+                validate();
+                if (getTopLevelAncestor()!=null) ((JFrame)getTopLevelAncestor()).pack();
+            }
+        });
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        JPanel p = new JPanel();
+        JLabel scripts = new JLabel(rb.getString("ScriptDir"));
+        p.add(scripts);
+        p.add(scriptLocation);
+        p.add(bScript);
+        scriptLocation.setColumns(30);
+        scriptLocation.setText(FileUtil.getScriptsPath());
+        return p;
+    }
+    
+    private JPanel PrefLocation(){
+        JPanel p = new JPanel();
+        JLabel users = new JLabel(rb.getString("PrefDir"));
+        p.add(users);
+        p.add(userLocation);
+        userLocation.setColumns(30);
+        userLocation.setText(FileUtil.getUserFilesPath());
+        
+        JButton bUser = new JButton(rb.getString("ButtonSetDots"));
+        final JFileChooser fcUser;
+        fcUser = new JFileChooser(FileUtil.getUserFilesPath());
+
+        fcUser.setDialogTitle(rb.getString("MessageSelectDirectory"));
+        fcUser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        fcUser.setAcceptAllFileFilterUsed(false);
+        bUser.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // get the file
+                fcUser.showOpenDialog(null);
+                if (fcUser.getSelectedFile()==null) return; // cancelled
+                userLocation.setText(fcUser.getSelectedFile()+File.separator);
+                isDirty = true;
+                validate();
+                if (getTopLevelAncestor()!=null) ((JFrame)getTopLevelAncestor()).pack();
+            }
+        });
+        p.add(bUser);
+        return p;
+    }
+
+    private static final JTextField scriptLocation = new JTextField();
+    private static final JTextField userLocation = new JTextField();
+    //protected static JTextField throttleLocation = new JTextField();
+
+    @Override
+    public String getPreferencesItem() {
+        return "FILELOCATIONS"; // NOI18N
+    }
+
+    @Override
+    public String getPreferencesItemText() {
+        return rb.getString("MenuFileLocation"); // NOI18N
+    }
+
+    @Override
+    public String getTabbedPreferencesTitle() {
+        return rb.getString("TabbedLayoutFileLocations"); // NOI18N
+    }
+
+    @Override
+    public String getLabelKey() {
+        return rb.getString("LabelTabbedFileLocations"); // NOI18N
+    }
+
+    @Override
+    public JComponent getPreferencesComponent() {
+        return this;
+    }
+
+    @Override
+    public boolean isPersistant() {
+        return true;
+    }
+
+    @Override
+    public String getPreferencesTooltip() {
+        return null;
+    }
+
+    @Override
+    public void savePreferences() {
+        FileLocationPane.save();
+    }
+
+    @Override
+    public boolean isDirty() {
+        return isDirty;
+    }
+
+}
+
---
+++ jake/split_programmer/java/src/jmri/jmrit/operations/trains/SetupExcelProgramSwitchListFrame.java
@@ -0,0 +1,124 @@
+// SetupExcelProgramSwitchListFrame.java
+
+package jmri.jmrit.operations.trains;
+
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
+import java.text.MessageFormat;
+import java.util.ResourceBundle;
+
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import jmri.jmrit.operations.OperationsFrame;
+import jmri.jmrit.operations.OperationsManager;
+import jmri.jmrit.operations.setup.Control;
+import jmri.jmrit.operations.setup.Setup;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Frame for user edit of the name and directory of an Excel program used for switch lists.
+ * 
+ * @author Dan Boudreau Copyright (C) 2013, 2014
+ * @version $Revision: 22249 $
+ */
+
+public class SetupExcelProgramSwitchListFrame extends OperationsFrame {
+
+	// text windows
+	JTextField fileName = new JTextField(30);
+	
+	// checkboxes
+	protected static final ResourceBundle rb = ResourceBundle
+			.getBundle("jmri.jmrit.operations.setup.JmritOperationsSetupBundle");
+	JCheckBox generateCvsSwitchListCheckBox = new JCheckBox(rb.getString("GenerateCsvSwitchList")); 
+
+	// major buttons
+	JButton testButton = new JButton(Bundle.getMessage("Test"));
+	JButton saveButton = new JButton(Bundle.getMessage("Save"));
+
+	public void initComponents() {
+
+		getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+		// Layout the panel by rows
+		// row 1
+		
+		JPanel pOptions = new JPanel();
+		pOptions.setBorder(BorderFactory.createTitledBorder(Bundle.getMessage("Options")));
+		pOptions.add(generateCvsSwitchListCheckBox);
+		
+		generateCvsSwitchListCheckBox.setSelected(Setup.isGenerateCsvSwitchListEnabled());
+
+		// row 3
+		JPanel pDirectoryName = new JPanel();
+		pDirectoryName.setBorder(BorderFactory.createTitledBorder(Bundle.getMessage("Directory")));
+		pDirectoryName.add(new JLabel(OperationsManager.getInstance().getFile(TrainCustomSwitchList.getDirectoryName()).getPath()));
+
+		JPanel pFileName = new JPanel();
+		pFileName.setBorder(BorderFactory.createTitledBorder(Bundle.getMessage("FileName")));
+		pFileName.add(fileName);
+
+		fileName.setText(TrainCustomSwitchList.getFileName());
+
+		// row 4 buttons
+		JPanel pButtons = new JPanel();
+		pButtons.setLayout(new GridBagLayout());
+		addItem(pButtons, testButton, 1, 0);
+		addItem(pButtons, saveButton, 3, 0);
+
+		getContentPane().add(pOptions);
+		getContentPane().add(pDirectoryName);
+		getContentPane().add(pFileName);
+		getContentPane().add(pButtons);
+
+		// setup buttons
+		addButtonAction(testButton);
+		addButtonAction(saveButton);
+
+		addHelpMenu("package.jmri.jmrit.operations.Operations_SetupExcelProgram", true); // NOI18N
+		setTitle(Bundle.getMessage("MenuItemSetupExcelProgram"));
+
+		initMinimumSize(new Dimension(Control.panelWidth400, Control.panelHeight300));
+	}
+
+	// Save and Test
+	public void buttonActionPerformed(java.awt.event.ActionEvent ae) {
+
+		TrainCustomSwitchList.setFileName(fileName.getText());
+
+		if (ae.getSource() == testButton) {
+			if (TrainCustomSwitchList.manifestCreatorFileExists()) {
+				JOptionPane.showMessageDialog(this, MessageFormat.format(Bundle.getMessage("DirectoryNameFileName"),
+						new Object[] { TrainCustomSwitchList.getDirectoryName(), TrainCustomSwitchList.getFileName() }),
+						Bundle.getMessage("ManifestCreatorFound"), JOptionPane.INFORMATION_MESSAGE);
+			} else {
+				JOptionPane.showMessageDialog(this, MessageFormat.format(
+						Bundle.getMessage("LoadDirectoryNameFileName"), new Object[] {
+								TrainCustomSwitchList.getDirectoryName(), TrainCustomSwitchList.getFileName() }), Bundle
+						.getMessage("ManifestCreatorNotFound"), JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		if (ae.getSource() == saveButton) {
+			log.debug("Save button activated");
+			Setup.setGenerateCsvSwitchListEnabled(generateCvsSwitchListCheckBox.isSelected());
+			TrainManagerXml.instance().writeOperationsFile();
+			if (Setup.isCloseWindowOnSaveEnabled())
+				dispose();
+		}
+	}
+
+	public void dispose() {
+		super.dispose();
+	}
+
+	static Logger log = LoggerFactory.getLogger(SetupExcelProgramSwitchListFrame.class.getName());
+}
