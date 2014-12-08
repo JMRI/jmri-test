11d4a2e68b78005e7268d96e6124167889f91719
Randall Wood <randall.h.wood@alexandriasoftware.com>
2014-12-08 22:40:47 +0000

    Whitespace and formatting changes.

--- java/src/jmri/jmrit/simpleprog/SimpleProgAction.java
+++ java/src/jmri/jmrit/simpleprog/SimpleProgAction.java
@@ -1,52 +1,54 @@
 // SimpleProgAction.java
+package jmri.jmrit.simpleprog;
 
- package jmri.jmrit.simpleprog;
-
- import jmri.util.swing.JmriAbstractAction;
-import jmri.util.swing.WindowInterface;
-import javax.swing.Icon;
 import java.awt.event.ActionEvent;
+import javax.swing.Icon;
+import jmri.util.swing.JmriAbstractAction;
+import jmri.util.swing.WindowInterface;
 
 /**
- * Swing action to create and register a
- *       			SimpleProgAction object
+ * Swing action to create and register a SimpleProgAction object
  *
- * @author			Bob Jacobsen    Copyright (C) 2001, 2008
- * @version			$Revision$
- */public class SimpleProgAction 			extends JmriAbstractAction {
+ * @author	Bob Jacobsen Copyright (C) 2001, 2008
+ * @version	$Revision$
+ */
+public class SimpleProgAction extends JmriAbstractAction {
 
     public SimpleProgAction(String s, WindowInterface wi) {
         super(s, wi);
     }
-     
+
     public SimpleProgAction(String s, Icon i, WindowInterface wi) {
         super(s, i, wi);
     }
-     public SimpleProgAction(String s) {
-         super(s);
 
-     // disable ourself if programming is not possible
-        if (jmri.InstanceManager.programmerManagerInstance()==null) { 
+    public SimpleProgAction(String s) {
+        super(s);
+
+        // disable ourself if programming is not possible
+        if (jmri.InstanceManager.programmerManagerInstance() == null) {
             setEnabled(false);
         }
-     }
-     
-     public SimpleProgAction() {
+    }
+
+    public SimpleProgAction() {
         this(Bundle.getMessage("MenuItemSingleCVProgrammer"));
-     }
-     
-     public void actionPerformed(ActionEvent e) {
-         
-         // create a SimpleProgFrame
-         SimpleProgFrame f = new SimpleProgFrame();
-         f.setVisible(true);
-         
-     }
-     
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        // create a SimpleProgFrame
+        SimpleProgFrame f = new SimpleProgFrame();
+        f.setVisible(true);
+
+    }
+
     // never invoked, because we overrode actionPerformed above
+    @Override
     public jmri.util.swing.JmriPanel makePanel() {
         throw new IllegalArgumentException("Should not be invoked");
     }
- }
+}
 
 /* @(#)SimpleProgAction.java */
