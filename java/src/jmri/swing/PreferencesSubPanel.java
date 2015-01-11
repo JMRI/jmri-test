fcb1bb0ffac4f07cb5d12a0dfa0016c1e8078d6b
Randall Wood <randall.h.wood@alexandriasoftware.com>
2015-01-11 10:24:06 +0000

    Documentation.

--- java/src/jmri/swing/PreferencesSubPanel.java
+++ java/src/jmri/swing/PreferencesSubPanel.java
@@ -1,5 +1,15 @@
 package jmri.swing;
 
+/**
+ * Additional methods that a PreferencesPanel that implements a GUI for another,
+ * different PreferencesPanel (called the parent) needs to implement for the
+ * {@link apps.gui3.TabbedPreferences} Preferences Window to correctly save
+ * preferential changes. These methods make it possible for TabbedPreferences to
+ * ensure the parent PreferencesPanel is created and correctly associated with
+ * this panel.
+ *
+ * @author Randall Wood <randall.h.wood@alexandriasoftware.com>
+ */
 public interface PreferencesSubPanel extends PreferencesPanel {
 
     public abstract String getParentClassName();
