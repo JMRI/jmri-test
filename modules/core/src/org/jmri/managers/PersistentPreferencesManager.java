bbd198749d0b0766a0051cdd8d8c7d6dd88e7cde
Randall Wood <randall.h.wood@alexandriasoftware.com>
2015-05-18 11:17:57 +0000

    Rely on TabbedPreferences while creating an API to replace it.

--- modules/core/src/org/jmri/managers/PersistentPreferencesManager.java
+++ modules/core/src/org/jmri/managers/PersistentPreferencesManager.java
@@ -1,6 +1,8 @@
 package org.jmri.managers;
 
-import apps.AppConfigBase;
+import apps.gui3.TabbedPreferences;
+import java.util.HashMap;
+import jmri.InstanceManager;
 import jmri.swing.PreferencesPanel;
 import org.openide.util.lookup.ServiceProvider;
 
@@ -13,38 +15,57 @@ import org.openide.util.lookup.ServiceProvider;
  * @author Randall Wood
  */
 @ServiceProvider(service = PersistentPreferencesManager.class)
-public class PersistentPreferencesManager extends AppConfigBase {
+public class PersistentPreferencesManager {
 
+    private final HashMap<String, PreferencesPanel> preferencesPanels = new HashMap<>();
     private static final long serialVersionUID = -9062945353256849209L;
 
     public PreferencesPanel getPanel(String clazz) {
-        return this.preferencesPanels.get(clazz);
+        return this.getTabbedPreferences().getPreferencesPanels().get(clazz);
     }
 
-    public void setPanel(PreferencesPanel panel) {
-        this.getPreferencesPanels().put(panel.getClass().getName(), panel);
-        if (panel.isPersistant()) {
-            items.add(panel);
-        }
+    public void putPanel(PreferencesPanel panel) {
+        this.getTabbedPreferences().getPreferencesPanels().put(panel.getClass().getName(), panel);
     }
 
     public void savePreferences() {
         this.savePreferences(this.isRestartRequired());
     }
 
+    /*
+     * This needs to replicate AppConfigBase.savePressed(boolean) to function 
+     * independently of TabbedPreferences. Note that for JMRI applications to 
+     * function correctly once that has been done, AppConfigBase will need to
+     * call Lookup.getDefault().getLookup(PersistentPreferencesManager.class).getPreferencesPanels()
+     * to function correctly. This has not been done as of now since the JMRI
+     * Core module depends on the JMRI Library module.
+     */
     public void savePreferences(boolean isRestartRequired) {
-        this.savePressed(isRestartRequired);
+        this.getTabbedPreferences().savePressed(isRestartRequired);
     }
 
     public boolean isRestartRequired() {
-        return this.preferencesPanels.values().stream().anyMatch((panel) -> (panel.isRestartRequired()));
+        return this.getTabbedPreferences().getPreferencesPanels().values().stream().anyMatch((panel) -> (panel.isRestartRequired()));
     }
 
     public boolean isDirty() {
-        return this.preferencesPanels.values().stream().anyMatch((panel) -> (panel.isDirty()));
+        return this.getTabbedPreferences().getPreferencesPanels().values().stream().anyMatch((panel) -> (panel.isDirty()));
     }
 
     public boolean isPreferencesValid() {
-        return this.preferencesPanels.values().stream().anyMatch((panel) -> (panel.isPreferencesValid()));
+        return this.getTabbedPreferences().getPreferencesPanels().values().stream().anyMatch((panel) -> (panel.isPreferencesValid()));
+    }
+
+    protected HashMap<String, PreferencesPanel> getPreferencesPanels() {
+        return this.preferencesPanels;
+    }
+
+    /*
+     * Remove this method and all references to it to break the dependency on 
+     * TabbedPreferences. All TabbedPreferences methods called by this class
+     * have one-to-one analogs with identical names already.
+     */
+    private TabbedPreferences getTabbedPreferences() {
+        return InstanceManager.getDefault(TabbedPreferences.class);
     }
 }
